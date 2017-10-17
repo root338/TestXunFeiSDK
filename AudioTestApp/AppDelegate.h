a6cf434bfadd3df21e0a01cb46f72ffe113c9fc6
DY <dy@yuemei.com>
2017-10-17 17:43:30 +0800

    Initial Commit

---
+++ AudioTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioTestApp
+//
+//  Created by DY on 2017/10/17.
+//  Copyright © 2017年 DY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
